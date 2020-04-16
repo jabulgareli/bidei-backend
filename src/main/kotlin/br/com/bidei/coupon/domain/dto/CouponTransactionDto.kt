a2278c5200d502b013d88b5e75d6754fbbd63c25
João Antonio Bulgareli <joao.bulgareli@beblue.com.br>
2020-04-16 20:41:35 -0300

    Adição do tipo no cupom

--- src/main/kotlin/br/com/bidei/coupon/domain/dto/CouponTransactionDto.kt
+++ src/main/kotlin/br/com/bidei/coupon/domain/dto/CouponTransactionDto.kt
@@ -2,12 +2,14 @@ package br.com.bidei.coupon.domain.dto
 
 import br.com.bidei.coupon.domain.model.Coupon
 import br.com.bidei.coupon.domain.model.CouponTransaction
+import br.com.bidei.coupon.domain.model.CouponType
 import java.util.*
 
 class CouponTransactionDto (
         val id: UUID,
         val code: String,
-        val campaignName: String
+        val campaignName: String,
+        val type: CouponType
 
 ){
     object Map {
@@ -15,7 +17,8 @@ class CouponTransactionDto (
                 CouponTransactionDto(
                         couponTransaction.coupon.id!!,
                         couponTransaction.coupon.code,
-                        couponTransaction.coupon.campaignName
+                        couponTransaction.coupon.campaignName,
+                        couponTransaction.coupon.type
                 )
     }
 }
